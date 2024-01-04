eafc1d03e58278b059d70c5b9bd9bed34106b4a9
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-01-04 12:13:34 +0300

    d

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -4,63 +4,24 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 const BASE_URL = "https://yeni.isleronline.com/uploads/assets/soruhavuzu_test/isleronline-{}.pdf"
 
-    bot.start(ctx => {
-        console.log("Received /start command")
-        try {
-            return ctx.reply("Hi")
-        } catch (e) {
-            console.error("error in start action:", e)
-            return ctx.reply("Error occured")
-        }
-    })
-
-    bot.help((ctx) => {
-        ctx.reply('Send /start to receive a greeting')
-        ctx.reply('Send /keyboard to receive a message with a keyboard')
-        ctx.reply('Send /quit to stop the bot')
-    })
-
-  bot.command('getpdf', (ctx) => {
-    // Simulate sending URLs for checking
-    for (let value = 1621844600; value < 1621844700; value++) {
-      checkAndNotify(ctx, value)
+bot.start(ctx => {
+    console.log("Received /start command")
+    try {
+        return ctx.reply("Hi")
+    } catch (e) {
+        console.error("error in start action:", e)
+        return ctx.reply("Error occured")
     }
-  })
+})
 
-  function checkAndNotify(ctx, value) {
-    const url = BASE_URL.replace('{}', value)
-  
-    fetch(url)
-      .then(response => response.text())
-      .then(content => {
-        // Check if the content is not empty and does not contain "Not Found"
-        if (content && !content.includes("Not Found")) {
-          console.log(`Valid URL: ${url}\nText Content Length: ${content.length}`);
-  
-          // Send a message to the user with inline keyboard
-          const keyboard = Markup.inlineKeyboard([
-            Markup.button.callback('Approve', `approve_${value}`),
-            Markup.button.callback('Reject', `reject_${value}`)
-          ])
-  
-          const message = `Valid URL: ${url}\nText Content Length: ${content.length}\n\nWhat do you want to do?`
-          ctx.reply(message, keyboard)
-        }
-      })
-      .catch(error => {
-        console.error(`Error accessing ${url}: ${error}`)
-      });
-  }
+bot.help((ctx) => {
+    ctx.reply('Send /start to receive a greeting')
+    ctx.reply('Send /keyboard to receive a message with a keyboard')
+    ctx.reply('Send /quit to stop the bot')
+})
 
-  bot.action(/(approve|reject)_\d+/, (ctx) => {
-    const [action, value] = ctx.match[0].split('_')
-  
-    // Implement your logic here based on the user's choice
-    ctx.reply(`You chose to ${action} URL ${value}`)
-  });
-  
   // Start the bot
-  bot.launch();
+bot.launch()
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 exports.handler = async event => {
