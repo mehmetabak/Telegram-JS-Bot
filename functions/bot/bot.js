5483d77b01e9ba4c50294bb68f48b361dc995452
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-01-04 10:48:30 +0300

    de

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -12,6 +12,22 @@ bot.start(ctx => {
   }
 })
 
+bot.help((ctx) => {
+    ctx.reply('Send /start to receive a greeting');
+    ctx.reply('Send /keyboard to receive a message with a keyboard');
+    ctx.reply('Send /quit to stop the bot');
+});
+
+bot.command('keyboard', (ctx) => {
+    ctx.reply(
+      'Keyboard',
+      Markup.inlineKeyboard([
+        Markup.button.callback('First option', 'first'),
+        Markup.button.callback('Second option', 'second'),
+      ])
+    );
+  });
+
 bot.command('cat', ({ replyWithPhoto }) => replyWithPhoto(randomPhoto))
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
