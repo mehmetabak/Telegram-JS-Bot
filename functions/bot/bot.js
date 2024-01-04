b940777e93d8ae81f8456b7120af4dd0edf58cc8
memoli0 <70433403+memoli0@users.noreply.github.com>
2024-01-04 11:20:10 +0300

    de

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,6 +1,21 @@
 const { Telegraf } = require("telegraf")
+const Extra = require('telegraf/extra')
+const session = require('telegraf/session')
+const { reply, fork } = Telegraf
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const randomPhoto = 'https://picsum.photos/200/300/?random'
+
+bot.use(session())
+
+// Register logger middleware
+bot.use((ctx, next) => {
+    const start = new Date()
+    return next().then(() => {
+      const ms = new Date() - start
+      console.log('response time %sms', ms)
+    })
+  })
 
 bot.start(ctx => {
   console.log("Received /start command")
